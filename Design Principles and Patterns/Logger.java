bce387f5b3ee8e16870538832c08b9d32aad4187
Naveenthiagarajan <149649195+Naveenthiagarajan@users.noreply.github.com>
2026-07-01 19:56:31 +0530

    Add files via upload

---
+++ Design Principles and Patterns/Logger.java
@@ -0,0 +1,35 @@
+class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger object created");
+    }
+
+    public static Logger getInstance() {
+
+        if (instance == null) {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application Started");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("User Logged In");
+
+        System.out.println(logger1 == logger2);
+    }
+}
